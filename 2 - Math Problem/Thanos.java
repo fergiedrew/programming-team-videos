b273f371bf9f8210a807faacd43804ed7cfef199
Andy Ferguson <ajferguson@udallas.edu>
2023-01-06 17:56:10 -0500

    Math Problem

--- 2 - Math Problem/Thanos.java
+++ 2 - Math Problem/Thanos.java
@@ -1,21 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 class Thanos {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int t = Integer.parseInt(scanner.nextLine());
+    public static void main(String[] args)  throws IOException {
+        int x = 9999 * 9999999;
+        System.out.println(x);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(reader.readLine());
         for (int i = 0; i < t;  i++) {
-            String input = scanner.nextLine();
+            String input = reader.readLine();
             String[] splitInput = input.split(" ");
             int initialPop = Integer.parseInt(splitInput[0]);
             int growthRate = Integer.parseInt(splitInput[1]);
             int foodSupply = Integer.parseInt(splitInput[2]);
-            int newPop = initialPop;
+            double newPop = (double)initialPop;
             int numYears = 0;
     
             while(newPop <= foodSupply) {
-                numYears += 1;
-                newPop *= growthRate;
+                numYears++;
+                newPop = newPop * growthRate;
             }
     
             System.out.println(numYears);
